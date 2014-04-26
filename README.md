ea8d3c692c63bcb4665a05f5d827ef02ac741f01
Connor Grady <conair360@gmail.com>
2014-04-26 19:33:27 -0400

    Switched to socket.io for page reloads and fixed issue #1.

--- README.md
+++ README.md
@@ -10,11 +10,24 @@ Markdown compiler + live reload written in nodejs, great for quick-editing markd
 
 ## Usage
 
-Just run `markdown-watch myfile.md` and open
-http://localhost:8080/myfile.html in your web browser.
+Just run `markdown-watch` and open http://localhost:8080/myfile in your web browser.
 
-If you have livereload then each time the relevant markdown
-file is saved, the browser will reload with the update.
+Each time the relevant markdown file is saved, the browser will reload with the updated rendered markdown.
+
+**NOTE:** All file paths are relative to your current working directory.
+
+
+## Options
+
+Currently, there are a few options you can apply.
+
+Invoking `markdown-watch --help` will list all the options
+available for your installed version of this package.
+
+### Port
+
+You can change the http listening port (defaults to 8080) by invoking the following:
+`markdown-watch -p 9090`
 
 
 
--- markdown-watch.js
+++ markdown-watch.js
@@ -1,69 +1,149 @@
 
+/**
+ * Module Dependencies
+ * 
+ * fs 						filesystem access
+ * path 					domain + folder + extension manipulation
+ * http 					http server
+ * 
+ * _ 						array + collection tools
+ * program 					cli tools to parse arguments, etc.
+ * marked					markdown to html parser
+ * Handlebars 				HTML templating module
+ * HTMLtemplate 			Easy function to compile + return the html template
+ * FileSocketCollection 	Object that stores all sockets and file watchers
+ * info						package.json file as an object
+ * 
+ */
 var fs = require('fs'),
+	path = require('path'),
+	http = require('http'),
 	_ = require('lodash'),
-	connect = require('connect'),
-	serveStatic = require('serve-static'),
-	gulp = require('gulp'),
-	watch = require('gulp-watch'),
-	plumber = require('gulp-plumber'),
-	markdown = require('gulp-markdown'),
-	livereload = require('gulp-livereload'),
-	args = process.argv.slice(2);
+	program = require('commander'),
+	marked = require('marked'),
+	Handlebars = require('handlebars'),
+	HTMLtemplate = Handlebars.compile( fs.readFileSync(path.join(__dirname, 'http/index.html'), {encoding: 'utf8'}) ),
+	FileSocketCollection = require('./lib/FileSocketCollection'),
+	info = require('./package.json');
 
 
 
 
-// Stub process.stdout.write
-var stdout_write = process.stdout.write;
-process.stdout.write = function () {}
-
-// Restore process.stdout.write
-//process.stdout.write = stdout_write;
+var LiveFiles = new FileSocketCollection;
 
 
+program
+	.version( info.version )
+	.option('-p --port <n>', 'HTTP listening port', parseInt, 8080);
 
-var LOG = function (value) {
-
-	if (stdout_write)
-		process.stdout.write = stdout_write;
-
-	console.log.apply(this, arguments);
-
-	stdout_write = process.stdout.write;
-
-	process.stdout.write = function () {}
-
-}
+program.parse(process.argv);
 
 
 
 
 
-// Check for http folder
-if (!fs.existsSync('http'))
-	fs.mkdirSync('http');
 
 
-var app = connect();
-app.use( serveStatic('http') );
-app.use(function (req, res) {
-	res.statusCode = 404;
-	res.end('Unknown resource!');
+
+
+
+
+
+
+
+
+
+
+
+
+var server = http.createServer(function (req, res) {
+
+	var filepath = path.join(process.cwd(), req.url)
+						.replace(path.extname(req.url), '')
+						.concat('.md'),
+		data = { port: program.port };
+
+	if ( fs.existsSync(filepath) ) {
+
+		res.statusCode = 200;	 // OK (202)
+
+		var markdown = marked( fs.readFileSync(filepath, {encoding: 'utf8'}) );
+
+		data.body = new Handlebars.SafeString( markdown );
+
+	}
+	else res.statusCode = 404;  // Not Found (404)
+
+	res.setHeader('Content-Type', 'text/html');
+	res.end( HTMLtemplate(data) );
+
 });
-app.listen(8080);
 
 
-var files = _.filter(args, fs.existsSync);
-gulp.src(files, { read: false })
-	.pipe( watch() )
-	.pipe( plumber() )
-	.pipe( markdown() )
-	.pipe( gulp.dest('http') )
-	.pipe( livereload() )
-	//.on('data', function () {
-	//	LOG('DATA!');
-	//});
 
 
-LOG('Listening...');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** Setup the socket.io server that all
+  * clients will use to be notified of
+  * relevant file changes to trigger a
+  * page reload when file is changed.
+  */
+var socketIO = require('socket.io').listen(server, {log: false});
+
+socketIO.on('connection', function (socket) {
+
+	/** When client connects, it sends a 'watch'
+	  * event with a relative path to the desired
+	  * file.
+	  */
+	socket.on('watch', function (filepath) {
+
+		filepath = path.join(process.cwd(), filepath)
+						.replace(path.extname(filepath), '')
+						.concat('.md');
+
+		try {
+			LiveFiles.add(filepath, socket);
+		}
+		catch (error) {
+			//socket.disconnect();
+			socket.emit('reload');  // Reload will display 404 page. A bit hacky, but works for now...
+		}
+
+	});
+
+});
+
+
+
+
+
+
+server.listen(program.port, function () {
+
+	console.log('Listening on port %s...', program.port);
+
+});
+
+
+
+
 
